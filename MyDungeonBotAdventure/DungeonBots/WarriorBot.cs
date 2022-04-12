193f648006248732b6aad7e23e466eaade5e83e6
James Schafer <james@jschafer.net>
2022-04-11 23:35:34 -0400

    Initial DungeonBot Template for getting started with the game.

---
+++ MyDungeonBotAdventure/DungeonBots/WarriorBot.cs
@@ -0,0 +1,15 @@
+﻿using DungeonBotGame.Combat;
+using DungeonBotGame.Foundation;
+
+namespace MyDungeonBotAdventure.DungeonBots;
+
+[DungeonBot("WarriorBot001")]
+public partial class WarriorBot
+{
+    public IAction Action(ISensorComponent sensorComponent)
+    {
+        var enemy = sensorComponent.Enemies.First();
+
+        return Attack(enemy);
+    }
+}
---
+++ MyDungeonBotAdventure/Program.cs
@@ -0,0 +1,1 @@
+DungeonBotGame.Application.Run(args);
